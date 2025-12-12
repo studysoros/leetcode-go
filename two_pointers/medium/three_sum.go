70f5355b94718169b85a24bea17fa87bab51bb21
fatsummercat <homelnw77@gmail.com>
2025-12-12 21:28:00 +0700

    two pointers

---
+++ two_pointers/medium/three_sum.go
@@ -0,0 +1,34 @@
+func threeSum(nums []int) [][]int {
+	var res [][]int
+
+	if len(nums) < 3 {
+		return res
+	}
+
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left, right := i+1, len(nums)-1
+		for left < right {
+			target := nums[i] + nums[left] + nums[right]
+			if target == 0 {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left, right = left+1, right-1
+
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+			} else if target > 0 {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+
+	return res
+}
